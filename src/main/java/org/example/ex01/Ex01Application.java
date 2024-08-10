b0661ab18d65254ecde0c10ab2781a3c834e8ad8
SO YEON LEE <thdus1323@gmail.com>
2024-08-11 07:29:39 +0900

    프로젝í트 만들ㅇ어보기

---
+++ src/main/java/org/example/ex01/Ex01Application.java
@@ -0,0 +1,13 @@
+package org.example.ex01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ex01Application {
+    // 쿠키런은 한글의 가독성이 매우 좋지요~~~
+    public static void main(String[] args) {
+        SpringApplication.run(Ex01Application.class, args);
+    }
+
+}

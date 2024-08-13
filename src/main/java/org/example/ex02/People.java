2e7c1a974fcdaa5dcce9c5e9945babd6a4d6cad3
SO YEON LEE <thdus1323@gmail.com>
2024-08-13 19:52:44 +0900

    생성자 공부

---
+++ src/main/java/org/example/ex02/People.java
@@ -0,0 +1,16 @@
+package org.example.ex02;
+
+import java.awt.*;
+
+public class People {
+    int hungryState = 50; // 배고픈 정도가 50 /100 중에
+
+    public void eat(){
+        hungryState += 10;
+    }
+
+   // public static void main(String[] args) {
+      //  People p = new People();
+       // p.eat();
+ //   }
+}
---
+++ src/main/java/org/example/ex02/TestApp.java
@@ -0,0 +1,8 @@
+package org.example.ex02;
+
+public class TestApp {
+    public static void main(String[] args) {
+        People 홍길동 = new People();
+        홍길동.eat();
+    }
+}

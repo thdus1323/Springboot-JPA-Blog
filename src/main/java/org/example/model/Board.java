a48d4672446259748a3a624f52cd6244cf023bb0
SO YEON LEE <thdus1323@gmail.com>
2024-08-14 12:58:35 +0900

    user구현중

---
+++ src/main/java/org/example/model/Board.java
@@ -0,0 +1,20 @@
+package org.example.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.sql.Timestamp;
+
+//@Builder
+//@AllArgsConstructor
+//@NoArgsConstructor
+//@Data
+//@Entity
+//public class Board {
+//
+//}
---
+++ src/main/java/org/example/model/User.java
@@ -0,0 +1,40 @@
+package org.example.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.sql.Timestamp;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //자동증가
+    private int id;
+
+    @Column(nullable = false, length = 30)
+    private String username;
+
+    @Column(nullable = false, length = 100) // 해시코드로 저장할 거라 넉넉하게!
+    private String password;
+
+    @Column(nullable = false, length = 50)
+    private String email;
+
+    @ColumnDefault("'user'")
+    private String role;
+
+    //자동시간입력
+    @CreationTimestamp
+    private Timestamp createDate;
+
+}

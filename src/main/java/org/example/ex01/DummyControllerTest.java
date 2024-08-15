158377d5b8ad4a7f6b18ae97a968e9b548a406fb
SO YEON LEE <thdus1323@gmail.com>
2024-08-15 21:42:49 +0900

    페이징

--- src/main/java/org/example/ex01/DummyControllerTest.java
+++ src/main/java/org/example/ex01/DummyControllerTest.java
@@ -5,12 +5,15 @@ import org.example.model.User;
 import org.example.respository.UserRepository;
 import org.hibernate.boot.model.naming.IllegalIdentifierException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.theme.CookieThemeResolver;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
 
+
+import java.util.List;
 import java.util.function.Supplier;
 
 @RestController
@@ -18,6 +21,24 @@ public class DummyControllerTest {
 
     @Autowired //DI
     private UserRepository userRepository;
+
+    @Transactional
+    //email, password
+    @PutMapping("/dummy/user/{id}")
+    public User updateUser(@PathVariable int id, @RequestBody User requestUser) {
+        System.out.println("id : " + id);
+        System.out.println("password: " + requestUser.getPassword());
+        System.out.println("email :" +requestUser.getEmail() );
+
+        User user = userRepository.findById(id).orElseThrow(()->{
+            return new IllegalArgumentException("수정에 실패하였습니다.");
+        });
+        user.setPassword(requestUser.getPassword());
+        user.setEmail(requestUser.getEmail());
+
+        //userRepository.save(user);
+        return null;
+    }
 
     //{id} 주소로 파라미터를 전달 받을 수 있다.
     //http://localhost:8000/blog/dummy/user/3
@@ -43,6 +64,20 @@ public class DummyControllerTest {
         //만약 자바 오브젝트를 리턴하게 되면 MessageConverter가 Jackson lib를 호출해서
         //user 오브젝트를 json으로 변환해서 브라우저에게 던져 줌.
         return user;
+    }
+
+    //http://localhost:8000/blog/dummy/user
+ //   @GetMapping("/dummy/user")
+   // public List<User> list(){
+  //      return userRepository.findAll();
+  //  }
+
+    //한페이지당 2건의 데이터를 리턴받아 볼 예정
+    @GetMapping("/dummy/user")
+    public List<User> pageList(@PageableDefault(size=2, sort = "id", direction = Sort.Direction.DESC)Pageable pageable){
+        Page<User> pagingUser = userRepository.findAll(pageable);
+        List<User> users= pagingUser.getContent();
+        return users;
     }
 
     //http://localhost:8000/blog/dummy/join(요청)

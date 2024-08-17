6e4aaf2715f2b442bf795ee58be057b3edf57a4d
SO YEON LEE <thdus1323@gmail.com>
2024-08-17 21:36:24 +0900

    회원가입 alert창

---
+++ src/main/resources/static/js/user.js
@@ -0,0 +1,20 @@
+let index = {
+    init: function (){
+        $("#btn-save").on("click",()=>{
+            this.save();
+        });
+    },
+
+    save: function(){
+        //alert('user의 save 함수 호출됨');
+        let data = {
+                username: $("#username").val(),
+                password: $("#password").val(),
+                email: $("#email").val(),
+        }
+
+       // console.log(data);
+    }
+}
+
+index.init();
